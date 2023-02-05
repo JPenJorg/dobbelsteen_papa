c2d7e42a0821ce51601276b6d73e0a62799f78ed
JPenJorg <124497137+JPenJorg@users.noreply.github.com>
2023-02-05 16:32:10 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,62 @@
+input.onGesture(Gesture.Shake, function () {
+    Nummer = randint(0, 5 + 1)
+    if (Nummer == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Nummer == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (Nummer == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (Nummer == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (Nummer == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+    basic.pause(1500)
+    basic.clearScreen()
+})
+let Nummer = 0
+basic.showLeds(`
+    # . # . #
+    # # # . .
+    # . # . #
+    . . . . #
+    . . . . #
+    `)
